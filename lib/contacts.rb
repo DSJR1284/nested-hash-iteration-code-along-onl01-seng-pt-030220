ef5f5dd7ca23c7ba613154e4d30bbdb8ad0c6511
Doug Jones <dsjr1284@gmail.com>
2020-03-15 13:40:47 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -22,3 +22,11 @@ require 'pry'
 #     end
 #   end 
 # end
+
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
+  end
+end
